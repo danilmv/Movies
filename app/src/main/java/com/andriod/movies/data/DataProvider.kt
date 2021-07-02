2fd077b441595a3cce66c527a2ce44fa440d1527
danilmv <43599357+danilmv@users.noreply.github.com>
2021-07-02 15:31:31 +0300

    L2:Movies: DataProvider

---
+++ app/src/main/java/com/andriod/movies/data/DataProvider.kt
@@ -0,0 +1,26 @@
+package com.andriod.movies.data
+
+import com.andriod.movies.entity.Movie
+import java.util.HashSet
+
+abstract class DataProvider {
+    private var subscribers: MutableSet<Runnable> = HashSet()
+    protected val data: MutableMap<String, Movie> = HashMap()
+
+    protected fun notifySubscribers() {
+        for (subscriber in subscribers) {
+            subscriber.run()
+        }
+    }
+
+    fun subscribe(subscriber: Runnable) {
+        subscribers.add(subscriber)
+    }
+
+    fun unSubscribe(subscriber: Runnable) {
+        subscribers.remove(subscriber)
+    }
+
+    abstract fun updateData(movie: Movie)
+    abstract fun findMovies(query: String): List<Movie>
+}
