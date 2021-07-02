54ddfb234dc6eabbb3affaf786364f7bac96ccc4
danilmv <43599357+danilmv@users.noreply.github.com>
2021-07-02 22:43:16 +0300

    L2:Movies: RecyclerView: binding fixing

--- app/src/main/java/com/andriod/movies/adapter/ListAdapter.kt
+++ app/src/main/java/com/andriod/movies/adapter/ListAdapter.kt
@@ -2,11 +2,8 @@ package com.andriod.movies.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.andriod.movies.R
 import com.andriod.movies.databinding.ItemMovieBinding
 import com.andriod.movies.entity.Movie
 
@@ -18,21 +15,8 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
             notifyDataSetChanged()
         }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding: ItemMovieBinding =
-            ItemMovieBinding.inflate(LayoutInflater.from(itemView.context))
-        private val textViewTitle = itemView.findViewById<TextView>(R.id.text_view_title)
-
-        fun bind(movie: Movie) {
-            Log.d(TAG, "bind() called with: movie = $movie")
-//            binding.textViewTitle.text = movie.title
-            textViewTitle.text = movie.title
-        }
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_movie, parent, false))
+        return ViewHolder(ItemMovieBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -42,5 +26,12 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     override fun getItemCount(): Int {
         Log.d(TAG, "getItemCount() called: list.size=${movies.size}")
         return movies.size
+    }
+
+    inner class ViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            Log.d(TAG, "bind() called with: movie = $movie")
+            binding.textViewTitle.text = movie.title
+        }
     }
 }
--- app/src/main/java/com/andriod/movies/fragment/ListFragment.kt
+++ app/src/main/java/com/andriod/movies/fragment/ListFragment.kt
@@ -6,14 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.andriod.movies.MyViewModel
-import com.andriod.movies.R
 import com.andriod.movies.adapter.ListAdapter
 import com.andriod.movies.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
-    private lateinit var binding: FragmentListBinding
+    private var binding: FragmentListBinding? = null
     private lateinit var adapter: ListAdapter
 
     override fun onCreateView(
@@ -21,23 +19,24 @@ class ListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return LayoutInflater.from(context).inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentListBinding.inflate(layoutInflater)
-        configureRecyclerView(view)
+        configureRecyclerView()
     }
 
-    private fun configureRecyclerView(view: View) {
-//        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+    private fun configureRecyclerView() {
         adapter = ListAdapter()
         adapter.movies = MyViewModel.movies.value?.values?.toList() ?: ArrayList()
-//        binding.recyclerView.adapter = adapter
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = adapter
+        binding?.recyclerView?.layoutManager = LinearLayoutManager(context)
+        binding?.recyclerView?.adapter = adapter
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
