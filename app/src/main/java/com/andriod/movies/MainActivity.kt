bd09a561f89b5099d7574fc46f3a112b3cefd500
danilmv <43599357+danilmv@users.noreply.github.com>
2021-07-06 19:33:34 +0300

    L3:Movies: bottom navigation

--- app/src/main/java/com/andriod/movies/MainActivity.kt
+++ app/src/main/java/com/andriod/movies/MainActivity.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import com.andriod.movies.entity.Movie
 import com.andriod.movies.fragment.MovieFragment
 import com.andriod.movies.fragment.MovieListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), MovieListFragment.MovieListContract {
     private var isLandscape = false
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,35 @@ class MainActivity : AppCompatActivity(), MovieListFragment.MovieListContract {
 
         isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
+        configureBottomView()
+        showList(false)
+    }
+
+    private fun configureBottomView() {
+        bottomNavigationView = findViewById(R.id.bottom_view)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_bottom_item_list -> {
+                    showList(false)
+                }
+                R.id.menu_bottom_item_favorites -> {
+                    showList(true)
+                }
+                R.id.menu_bottom_item_settings -> {
+                    showSettings()
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+            true
+        }
+    }
+
+    private fun showSettings() {
+    }
+
+    private fun showList(showFavorites: Boolean) {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.main_container, MovieListFragment())
@@ -23,7 +54,7 @@ class MainActivity : AppCompatActivity(), MovieListFragment.MovieListContract {
     }
 
     override fun changeMovie(movie: Movie) {
-        if (!isLandscape){
+        if (!isLandscape) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.main_container, MovieFragment.newInstance(movie))
