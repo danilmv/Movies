5b6b1b7aaba4606002828bd7228d8d6d7deef33b
danilmv <43599357+danilmv@users.noreply.github.com>
2021-07-06 16:09:55 +0300

    L3:Movies: protect MyViewModel.movies from change

--- app/src/main/java/com/andriod/movies/MyViewModel.kt
+++ app/src/main/java/com/andriod/movies/MyViewModel.kt
@@ -1,19 +1,21 @@
 package com.andriod.movies
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.andriod.movies.data.DummyDataProvider
 import com.andriod.movies.entity.Movie
 
 object MyViewModel {
-    val movies = MutableLiveData<MutableMap<String, Movie>>()
+    private val _movies = MutableLiveData<MutableMap<String, Movie>>()
+    val movies: LiveData<MutableMap<String, Movie>> = _movies
 
     fun initData() {
         if (movies.value?.isEmpty() != false) {
-            movies.value = HashMap()
+            _movies.value = HashMap()
 
             val dummy = DummyDataProvider()
             dummy.subscribe {
-                movies.value = dummy.data
+                _movies.postValue(dummy.data)
             }
         }
     }
