23bf5564facfe50cf91b53b3ecac17f84707bd50
danilmv <43599357+danilmv@users.noreply.github.com>
2021-07-22 23:10:40 +0300

    L5_: InternetAccess: status bar: timer-refresh

--- app/src/main/java/com/andriod/movies/statusbar/StatusBarView.kt
+++ app/src/main/java/com/andriod/movies/statusbar/StatusBarView.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import com.andriod.movies.databinding.StatusBarViewBinding
 import java.lang.StringBuilder
+import java.lang.Thread.sleep
 
 
 class StatusBarView : LinearLayout {
@@ -19,6 +20,10 @@ class StatusBarView : LinearLayout {
     private lateinit var statuses: LiveData<Map<Int, String>>
 
     private val groupIndexes = HashMap<Int, Int>()
+
+    private lateinit var showDataThread: Thread
+
+    private var lastTimeDataRefreshed: Long = 0
 
     constructor(context: Context?) : super(context) {
         initView(context)
@@ -67,8 +72,10 @@ class StatusBarView : LinearLayout {
 
     private fun getNextValueFromGroup(
         group: MutableMap.MutableEntry<Int, MutableSet<Int>>,
-        listOfValues: Map<Int, String>
+        listOfValues: Map<Int, String>,
     ): String? {
+        lastTimeDataRefreshed = System.currentTimeMillis()
+
         val groupId = group.key
         val groupValues = group.value.toList()
         groupIndexes[groupId] = groupIndexes[groupId]?.plus(1)?.rem(groupValues.size) ?: 0
@@ -79,7 +86,39 @@ class StatusBarView : LinearLayout {
         binding = StatusBarViewBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
+    private fun startShowDataThread() {
+        showDataThread = Thread {
+            while (!Thread.currentThread().isInterrupted) {
+                try {
+                    sleep(200)
+                } catch (e: InterruptedException) {
+                    break
+                }
+
+                if (System.currentTimeMillis() - lastTimeDataRefreshed >= REFRESH_TIME) {
+                    statuses.value?.let {
+                        if (it.isNotEmpty()) binding.textView.post {
+                            binding.textView.text = getStatusText(it)
+                        }
+                    }
+                }
+            }
+        }.apply { isDaemon = true }
+        showDataThread.start()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        startShowDataThread()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        showDataThread.interrupt()
+    }
+
     companion object {
         const val TAG = "@@StatusBarView"
+        const val REFRESH_TIME: Long = 1000
     }
 }
